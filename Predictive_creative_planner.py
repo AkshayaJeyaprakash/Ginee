fb0afa238498031f6176bae2c378fd8b404abb2c
Akshaya Jeyapraksh <86668046+AkshayaJeyaprakash@users.noreply.github.com>
2023-02-06 19:53:03 +0530

    Add files via upload

---
+++ Predictive_creative_planner.py
@@ -0,0 +1,16 @@
+class ginee:
+
+    def kill(ad_id, ctr, budget):
+        max_ctr = max(ctr)
+        index_max = ctr.index(max_ctr)
+        retain = ad_id[index_max]
+        return retain
+
+    def json_list(json):
+        return ad_id, ctr, budget
+
+    def dict_list(dict1):
+        return ad_id, ctr, budget
+
+    def excel_list(filename):
+        return ad_id, ctr, budget
